67f7701fdf59fe0904f8eeb101a0cf4ca24f49ed
Skaishgggg <swakaish444@gmail.com>
2025-02-26 09:34:13 +0530

    first commit

---
+++ Routes/Controller/userController.js
@@ -0,0 +1,5 @@
+exports.register =(req,res)=>{
+    console.log('inside register controller function');
+    res.status(200).json('Register Request Recieved')
+
+} 
---
+++ index.js
@@ -0,0 +1,25 @@
+// ithine import cheythale .evn file le content project le process lek load avu
+require('dotenv').config()  
+const express = require('express')
+const cors = require('cors')
+const router = require('./Routes/router')
+// create an express application
+const pfsever = express()
+
+
+
+
+pfsever.use(cors())
+pfsever.use(express.json())
+pfsever.use(router)
+ const PORT = 4000 || process.env.PORT
+
+ pfsever.listen(PORT,()=>{
+    console.log(`project fair server started at port :${PORT} and waiting for client request`);
+    
+ })
+
+ pfsever.get('/',(req,res)=>
+{
+    res.send(`<h1>project fair server started  and waiting for client request</h1>`)
+})
